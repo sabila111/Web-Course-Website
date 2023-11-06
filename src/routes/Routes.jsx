c9ef458254d6a7332b42abe5e94be4bc0e6ecb0e
sabila111 <sharminsharker2002@gmail.com>
2023-11-06 20:07:05 +0600

    made login page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import Main from "../layout/Main";
 import Home from "../pages/HOME/home/Home";
 import ErrorPage from "../errorPage/ErrorPage";
 import LogIn from "../pages/Shared/login/LogIn";
+import Register from "../pages/Shared/register/Register";
 
 const router = createBrowserRouter([
     {
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
         {
           path: "/login",
           element:<LogIn></LogIn>,
+        },
+        {
+          path: "/register",
+          element:<Register></Register>,
         },
       ],
     },

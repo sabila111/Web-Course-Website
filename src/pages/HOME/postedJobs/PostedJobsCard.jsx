438d40a28ab65d30c50f9d853e1abec3f652bffd
sabila111 <sharminsharker2002@gmail.com>
2023-11-07 20:29:16 +0600

    made delete button

--- src/pages/HOME/postedJobs/PostedJobsCard.jsx
+++ src/pages/HOME/postedJobs/PostedJobsCard.jsx
@@ -1,3 +1,4 @@
+import Swal from "sweetalert2";
 
 const PostedJobsCard = ({job}) => {
    
@@ -5,6 +6,24 @@ const PostedJobsCard = ({job}) => {
 
   const handleDelet =_id=>{
     console.log(_id)
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+        //   Swal.fire({
+        //     title: "Deleted!",
+        //     text: "Your file has been deleted.",
+        //     icon: "success"
+        //   });
+        console.log('delet')
+        }
+      });
   }
 
     return (

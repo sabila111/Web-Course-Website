404ffb15400775ceb3abf9ce5b39e4d3eff4aeb1
sabila111 <sharminsharker2002@gmail.com>
2023-11-08 17:54:44 +0600

    made banner page

--- src/pages/Shared/banner/Banner.jsx
+++ src/pages/Shared/banner/Banner.jsx
@@ -1,15 +1,13 @@
 
 const Banner = () => {
     return (
-        <div className="hero h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/ygj22vN/nick-morrison-FHnnjk1-Yj7-Y-unsplash.jpg)' }}>
+        <div className="hero h-screen " style={{ backgroundImage: 'url(https://i.ibb.co/ygj22vN/nick-morrison-FHnnjk1-Yj7-Y-unsplash.jpg)' }}>
             {/* <div className="hero-overlay bg-opacity-60"></div> */}
-            <div className="hero-content text-neutral-content ml-auto  mb-28">
-                <div className="max-w-md">
-                    <p className="text-slate-300 font-normal text-start mb-4 mr-28"># Spring 2023</p>
-                    <h1 className="mb-5 text-5xl font-bold text-start mr-28 text-white">T <span>R</span> <span>E</span> <span>N</span> <span>D</span> <span>Y</span></h1>
-                    <h1 className="mt-3 text-5xl font-bold text-end mr-20 text-white">S <span>H</span> <span>O</span> <span>P</span> </h1>
-                    <p className="mb-5 mt-5 text-start">Want a perfect pick for your classic collection? <br /> We are the best option </p>
-                    <button className="btn mr-64"> Shop Now</button>
+            <div className="hero-content text-neutral-content ">
+                <div className="max-w-5xl mr-[600px] mb-20 ">
+                    <h1 className="text-white text-6xl font-bold mb-3">Think Big. We Make <br /><span className="mt-5">It, Possible!</span></h1>
+                    <p className="mb-3 text-white text-base">We Observe The Ongoing Trends And analize the upcoming <br />technology to transform the digital person</p>
+                    <button className="py-3 px-4 rounded-lg bg-blue-900 font-bold text-white">Our Team</button>
                 </div>
             </div>
         </div>
